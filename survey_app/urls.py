c14cd8cb94e773d77f2e634dc3644bd1a2575fa4
kis <kiselev1189@gmail.com>
2015-08-29 01:09:50 +0300

    1

--- survey_app/urls.py
+++ survey_app/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<survey_id>\d+)/$', views.survey_detail, name='survey_detail'),
-    url(r'/register', views.register, name='register')
-
+    url(r'^register/', views.register, name='register'),
+    url(r'^login/', views.user_login, name='login'),
+    url(r'^my_surveys/', views.response_list, name='response_list')
 ]
--- survey_app/views.py
+++ survey_app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.template import RequestContext, loader
-from .models import Survey, UserProfile
+from .models import Survey, UserProfile, Response
 from .forms import ResponseForm, UserForm
 
 def index(request):
     survey_list = Survey.objects.all()
     context = {"survey_list":survey_list}
-    return render(request, 'survey_app/index.html', context)
+    return render(request, 'survey_app/survey_list.html', context)
 
 def survey_detail(request, survey_id):
     survey = Survey.objects.get(id=survey_id)
@@ -47,3 +49,31 @@ def register(request):
         user_form = UserForm()
 
     return render(request, "survey_app/register.html", {"user_form": user_form})
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect("/index.html")
+            else:
+                return HttpResponse("Account is not active")
+
+        else:
+            return HttpResponse("Wrong password or login")
+
+    else:
+        return render(request, "survey_app/login.html")
+
+@login_required
+def response_list(request):
+    user_profile = UserProfile.objects.filter(user=request.user)
+    response_list = Response.objects.filter(user=user_profile)
+    context = {"response_list": response_list}
+    return render(request, 'survey_app/response_list.html', context)
